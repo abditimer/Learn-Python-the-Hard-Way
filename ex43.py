be3ef1c86ecde26b4f52bc0b0df9775d28450420
abditimer <thatimer@gmail.com>
2018-10-08 16:42:40 +0100

    completed game.

--- ex43.py
+++ ex43.py
@@ -1,12 +1,12 @@
 #High level overview of the classess needed:
 
-#Map
-	#-next_scene
-	#-opening_scene
+#Map: map controls the scenes you go into
+#	-next_scene
+#	-opening_scene
 #Engine
 	#-play
-#Scene
-	#-enter
+#Scene: scene class that have that entire scene.
+#   -enter: enter function that lets you enter the room
 	#Death
 	#Central Corridor
 	#Laser weapon armory
@@ -18,101 +18,137 @@
 ###
 
 
+from sys import exit
+from random import randint
+
 class Scene(object):
 	def enter(self):
-		pass
-
+		print "This scene is not yet configured. Subclass it and implement the error()."
+		exit(1)
 
 class Engine(object):
-	#Starting the Engine.
+
 	def __init__(self, scene_map):
 		self.scene_map = scene_map
-	
+
 	def play(self):
-		#Player wants to play the game - launch
-		self.scene_map.opening_scene()
-		self.scene_map.next_scene(self.scene_map.start_scene)
+		current_scene = self.scene_map.opening_scene()
 
-
-#Each room does:
-#1. Prints its own description
-#2. tells engine what room to run next.
-
-
+		while True:
+			print "\n----------"
+			next_scene_name = current_scene.enter()
+			current_scene = self.scene_map.next_scene(next_scene_name)
 
 class Death(Scene):
-	#Here player dies - funny death scenes (maybe cycle/randomise them.)
+	quips = [
+		"You died. You kinda suck at this.",
+		"Your mom would be proud...if she were smarter.",
+		"Such a luser.",
+		"I have a small puppy that's better at this."
+	]
+
 	def enter(self):
-		print "Death room entered. You Died. Try again."
-
-
-
+		print Death.quips[randint(0, len(self.quips)-1)]
+		exit(1)
 
 class CentralCorridor(Scene):
-	#Here the player needs to defeat the user with a funny joke.
 	def enter(self):
-		print "You have enetered the Central Corridor."
-		Death().enter()
-		
+		print ("You are on an alient spaceship. There is an alien infront of you")
+		print ("What do you do? Do you shoot, dodge or tell a joke?")
+		action = raw_input(">")
+
+		if action == "shoot":
+			print "You cant kill no gothon. He shoots you first and eats you."
+			return 'death'
+		elif action == 'dodge':
+			print "Gothon stomps on you, you die...and then he eats you."
+			return 'death'
+		elif action == 'tell a joke':
+			print "you made the gothon laugh! Then you shoot him in his ugly face."
+			print "Congrats, you now run into the weapons room!"
+			return 'laser_weapon_armory'
+		else:
+			print "DOES NOT COMPUTE"
+			return "central_corridor"
 
 
 class LaserWeaponArmory(Scene):
-	#Neutron bomb is in this room. He needs to guess the pass key number or blows up.
+
 	def enter(self):
-		print('Well done, you have now entered the Weapons Room.')
-		
+		print "guess the password to remove the neutron bomb."
+		print "Get the code right...."
+		code = "%d%d%d" % (randint(1,9), randint(1,9), randint(1,9))
+		guess = raw_input("[keypad]>")
+		guesses = 0
+		while guess != code and guesses < 10:
+			print "BZZZZZEED"
+			guesses += 1
+			guess = raw_input("[keypad]>")
+
+		if guess == code:
+			print "You did it! Now run to the bridge and place it!"
+			return 'the_bridge'
+		else:
+			print "Too late..."
+			return 'death'
+
 class TheBridge(Scene):
-	#battle scene where the hero must place the bomb
+
 	def enter(self):
-		print('Well done, you have now entered the Bridge Room.') 
+		print "You are now on the bridge, with some Goftons. What do you do?"
+		print "Do you throw the bomb or slowly place the bomb?"
+
+		action = raw_input("> ")
+
+		if action == "throw the bomb":
+			print "Too bad, the bomb goes off."
+			return 'death'
+		elif action == "slowly place the bomb":
+			print "you place the bomb and slowly move towards the lift."
+			print "you point your weapon at the bomb as you jump in the lift and close it"
+			print "you've escaped, and you're now on the way to the escape pods."
+			return 'escape_pod'
+		else:
+			print "DOES NOT COMPUTE"
+			return "the_bridge"
 
 class EscapePod(Scene):
-	#hero must escape, but only after guessing the right escape pod.
+
 	def enter(self):
-		print('Well done, you have now entered the Evacuation Room.')
+		print "pick a pod to escape, some are damaged."
 
+		good_pod = randint(1,5)
+		guess = raw_input("[pod #]> ")
 
-
-
-
+		if int(guess) != good_pod:
+			print "Your pod blew up."
+			return 'death'
+		else:
+			print "You made it! The aliens all died."
+			return 'finished'
 
 class Map(object):
+	scenes = {
+		'central_corridor': CentralCorridor(),
+		'laser_weapon_armory': LaserWeaponArmory(),
+		'the_bridge': TheBridge(),
+		'escape_pod': EscapePod(),
+		'death': Death()
+	}
+
 	def __init__(self, start_scene):
 		self.start_scene = start_scene
 
-	#Pass the next scene and the map state changes
-	def next_scene(self, sName):
-		"""
-		Depending where you are, go to the next room.
-		"""
-		if sName == 'central_corridor':
-			CentralCorridor().enter()
-		elif sName == 'Death':
-			Death().enter()
-		elif sName == 'LaserWeaponArmory':
-			LaserWeaponArmory().enter()
-		elif sName == 'TheBridge':
-			TheBridge().enter()
-		else:
-			EscapePod().enter()
+	def next_scene(self, scene_name):
+		return Map.scenes.get(scene_name)
 
 	def opening_scene(self):
-		"""
-		This scene introduces the player to the hero. The first time the user enters the game.
-		"""
-		print "Hello user. Welcome to the game. You will play a character caled:  Tim."
+		return self.next_scene(self.start_scene)
 
 
 
-
-#The game involves an engine
-#that runs a map full of scenes.
-
-#Create a Map object that has a load of scenes.
 a_map = Map('central_corridor')
-#Start the game engine that has the map we created
 a_game = Engine(a_map)
-#Play.
 a_game.play()
 
 
